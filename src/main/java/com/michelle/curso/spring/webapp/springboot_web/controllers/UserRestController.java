187cd57729367576fe295ba7bad6007f9d7956bc
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-01-31 09:08:15 -0400

    cambio a objeto user

--- src/main/java/com/michelle/curso/spring/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/michelle/curso/spring/webapp/springboot_web/controllers/UserRestController.java
@@ -9,17 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.michelle.curso.spring.webapp.springboot_web.models.User;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping(path="/details")
     public Map<String, Object> details() {
+        User user = new User("Michelle", "Barriga");
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola Mundo Spring Boot");
-        body.put("name", "Michelle");
-        body.put("lastname", "Barriga");
+        body.put("user", user);
+
         return body;
     }
 
