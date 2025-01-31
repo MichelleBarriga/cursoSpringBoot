701b2fef0345ea90ec247b786a048e7a8a1f9789
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-01-31 09:11:40 -0400

    Objeto es UserController

--- src/main/java/com/michelle/curso/spring/webapp/springboot_web/UserController.java
+++ src/main/java/com/michelle/curso/spring/webapp/springboot_web/UserController.java
@@ -4,15 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.michelle.curso.spring.webapp.springboot_web.models.User;
+
 @Controller
 
 public class UserController {
 
     @GetMapping("/details")
     public String details(Model model) {
+        User user = new User("Michelle", "Barriga");
         model.addAttribute("title", "Hola Mundo Spring Boot");
-        model.addAttribute("name", "Michelle");
-        model.addAttribute("lastname", "Barriga");
+        model.addAttribute("user", user);
         return "details";
     }
 
