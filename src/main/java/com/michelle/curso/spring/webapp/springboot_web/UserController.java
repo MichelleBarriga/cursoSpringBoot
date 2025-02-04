e6893c2f8bfdc144b5c3918f057b02543c3da19b
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-02-04 00:01:48 -0400

    listas con thymeleaf

--- src/main/java/com/michelle/curso/spring/webapp/springboot_web/UserController.java
+++ src/main/java/com/michelle/curso/spring/webapp/springboot_web/UserController.java
@@ -1,10 +1,16 @@
 package com.michelle.curso.spring.webapp.springboot_web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.michelle.curso.spring.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 
@@ -13,9 +19,20 @@ public class UserController {
     @GetMapping("/details")
     public String details(Model model) {
         User user = new User("Michelle", "Barriga");
+        user.setEmail("barriga.michelle@gmail.com");
         model.addAttribute("title", "Hola Mundo Spring Boot");
         model.addAttribute("user", user);
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users =  new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usarios");
+        return "list";
+    }
+    
 
 }
