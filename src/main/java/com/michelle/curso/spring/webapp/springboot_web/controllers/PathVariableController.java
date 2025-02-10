308d14169a853bb9b46821e98693ac164180eab8
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-02-09 23:18:26 -0400

    add varias variables con PathVariable

--- src/main/java/com/michelle/curso/spring/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/michelle/curso/spring/webapp/springboot_web/controllers/PathVariableController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.michelle.curso.spring.webapp.springboot_web.models.dto.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,5 +23,14 @@ public class PathVariableController {
         param.setMessage(message);
         return param;
     }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar( @PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        return json;
+    }
+    
     
 }
